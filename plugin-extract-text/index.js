af79168faf93ba01898e2167f7b2120dde4a1868
William Eduardo Meza <williameduardomeza@gmail.com>
2019-04-23 13:31:17 -0500

    plugins added

---
+++ plugin-extract-text/index.js
@@ -0,0 +1,4 @@
+import './estilos.css'
+document.write('Hello World')
+//console.log('Hello World, from webpack')
+console.log('Hello World, from extenal webpack config')
---
+++ plugin-extract-text/webpack.config.js
@@ -0,0 +1,30 @@
+const path = require('path')
+// camel case is required
+const ExtractTextPlugin = require('extract-text-webpack-plugin')
+
+module.exports = {
+    entry: path.resolve(__dirname, './index.js'),
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js'
+    },
+    module: {
+        rules: [
+            //here we put the loaders
+            {
+                // file type to read
+                test: /\.css$/,
+                // which loader is going to manage the file
+                use: ExtractTextPlugin.extract({
+                    //[ 'style-loader' ,'css-loader']
+                    fallback: "style-loader",
+                    use: "css-loader"
+                }),
+            }
+        ]
+    },
+    plugins: [
+        // need to require this first
+        new ExtractTextPlugin("css/[name].css")
+    ]
+}

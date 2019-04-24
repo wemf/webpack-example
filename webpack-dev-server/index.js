a58f357a68de0fe8ab350b51257881cd86054eba
William Eduardo Meza <williameduardomeza@gmail.com>
2019-04-23 22:00:11 -0500

    added Webpack DevServer (L11)

---
+++ webpack-dev-server/index.js
@@ -0,0 +1,4 @@
+import './estilos.css'
+document.write('Hello World, this is a third new change')
+//console.log('Hello World, from webpack')
+console.log('Hello World, from extenal webpack config')
---
+++ webpack-dev-server/webpack.config.js
@@ -0,0 +1,23 @@
+const path = require('path')
+
+module.exports = {
+    entry: path.resolve(__dirname, './index.js'),
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js'
+    },
+    devServer: {
+        port: 9000
+    },
+    module: {
+        rules: [
+            //here we put the loaders
+            {
+                // file type to read
+                test: /\.css$/,
+                // which loader is going to manage the file
+                use: ['style-loader' ,'css-loader']
+            }
+        ]
+    }
+}
